1a20f5dd5229526c49cb0341cfda18bd4f6d1888
abdellah404 <1234abdellah007@gmail.com>
2025-01-15 02:33:03 +0100

    profile edit

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,123 +1,152 @@
-const express = require("express");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
-const connectToDatabase = require("../models/db");
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const connectToDatabase = require('../models/db');
 const router = express.Router();
-const dotenv = require("dotenv");
-const pino = require("pino"); // Import Pino logger
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
 
-const logger = pino(); // Create a Pino logger instance
+//Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+const { body, validationResult } = require('express-validator');
+
+
+const logger = pino();  // Create a Pino logger instance
+
+dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post("/register", async (req, res) => {
-  try {
-    const db = await connectToDatabase();
-    const collection = await db.collection("users");
-    const user = await collection.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
+router.post('/register', async (req, res) => {
+    try {
+      //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+      const existingEmail = await collection.findOne({ email: req.body.email });
+
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email=req.body.email;
+        console.log('email is',email);
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User registered successfully');
+        res.json({ authtoken,email });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
     }
-    const hash = bcryptjs.hashSync(req.body.password, 10);
-    const newUser = await collection.insertOne({
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: hash,
-      createdAt: new Date(),
-    });
-
-    const payload = {
-      user: {
-        id: newUser.insertedId,
-      },
-    };
-
-    const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info("User registered successfully");
-    res.json({ authtoken, email: req.body.email });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error" + err);
-  }
 });
 
-
 router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
 
     try {
+        // const collection = await connectToDatabase();
         const db = await connectToDatabase();
-        const collection = await db.collection("users");
-        const user = await collection.findOne({ email: req.body.email });
-        if (!user) {
-            logger.error('User not found');
-            return res.status(400).json({ message: "User not found or not registred yet" });
-        }
-        else {
-            const isMatch = bcryptjs.compareSync(req.body.password, user.password);
-            if (!isMatch) {
+        const collection = db.collection("users");
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
                 logger.error('Passwords do not match');
-                return res.status(404).json({ message: "Invalid Password" });
+                return res.status(404).json({ error: 'Wrong pasword' });
             }
-            const payload = {
+            let payload = {
                 user: {
-                    id: user._id,
+                    id: theUser._id.toString(),
                 },
             };
+
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
             const authtoken = jwt.sign(payload, JWT_SECRET);
-            logger.info("User logged in successfully");
-            res.json({ authtoken, email: req.body.email , firstName: user.firstName });
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
         }
-            
-} catch (err) {
-    res.status(500).send("Internal Server Error" + err);
-}}
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
+});
 
-);
-
-
-
+// update API
 router.put('/update', async (req, res) => {
-    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error;
+    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+
     const errors = validationResult(req);
-if (!errors.isEmpty()) {
-  logger.error('Validation errors in update request', errors.array());
-  return res.status(400).json({ errors: errors.array() });
-}
 
-try {
     // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
-    const email = req.headers.email;
-
-    if (!email) {
-        logger.error('Email not found in the request headers');
-        return res.status(400).json({ error: "Email not found in the request headers" });
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
     }
-    // Task 4: Connect to MongoDB
-    const db = await connectToDatabase();
-    // Task 5: find user credentials in database
+
+    try {
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        //Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
         const collection = db.collection("users");
 
+        //Task 5: Find user credentials
+        const existingUser = await collection.findOne({ email });
 
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
 
-    existingUser.updatedAt = new Date();
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
 
-    // Task 6: update user credentials in database
-    const updatedUser = await collection.findOneAndUpdate(
-        { email },
-        { $set: existingUser },
-        { returnDocument: 'after' }
-    );    // Task 7: create JWT authentication using secret key from .env file
-    const payload = {
-        user: {
-            id: existingUser._id.toString(),
-        },
-    };
-    const authtoken = jwt.sign(payload, JWT_SECRET);
-    res.json({authtoken});
-} catch (e) {
-     return res.status(500).send('Internal server error');
+        //Task 6: Update user credentials in DB
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
 
-}
+        //Task 7: Create JWT authentication with user._id as payload using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+
+        res.json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
 });
 module.exports = router;
--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,55 +1,71 @@
 import React, { useState,useEffect } from 'react';
-import {useAppContext} from '../../context/AuthContext';
+//Step 1 - Task 1
+import {urlConfig} from '../../config';
+//Step 1 - Task 2
+import { useAppContext } from '../../context/AuthContext';
+//Step 1 - Task 3
 import { useNavigate } from 'react-router-dom';
-import { urlConfig } from '../../config';
-
 
 import './LoginPage.css';
 
 function LoginPage() {
-    const [ incorrect, setIncorrect] = useState("");  
-    const navigate = useNavigate();
-    const bearerToken = sessionStorage.getItem('auth-token');
-    const { setIsLoggedIn } = useAppContext();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    if(bearerToken ){
-        navigate('/app');
-    }
+    //Step 1 - Task 4
+    const [incorrect, setIncorrect] = useState('');
+    //Step 1 - Task 5
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    //Step 1 - Task 6
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
+
     const handleLogin = async (e) => {
-       
-        const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
+        e.preventDefault();
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            //Step 1 - Task 7
+            method: 'POST',
+            //Step 1 - Task 8
+          headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+        //Step 1 - Task 9
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
         });
-        const json = await response.json();
+
+        //Step 2: Task 1
+        const json = await res.json();
+        console.log('Json',json);
         if (json.authtoken) {
-            sessionStorage.setItem('auth-token', json.authtoken);
-            sessionStorage.setItem('name', json.firstName);
-            sessionStorage.setItem('email', json.email);
-            setIsLoggedIn(true);
-            navigate('/app');
+            //Step 2: Task 2
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            //Step 2: Task 3
+          setIsLoggedIn(true);
+            //Step 2: Task 4
+          navigate('/app');
         } else {
-            document.getElementById("email").value="";
-            document.getElementById("password").value="";
-            setIncorrect("Wrong email or password. Try again.");
-        //Below is optional, but recommended - Clear out error message after 2 seconds
-            setTimeout(() => {
-              setIncorrect("");
-            }, 2000);
-          }
+            //Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
 
-        
-
-
-
-}
+      }
 
 
     return (
@@ -66,7 +82,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                             />
                         </div>
                         <div className="mb-4">
@@ -77,11 +93,12 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                             />
-                           <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+
+                            {/*Step 2: Task 6*/}
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
-                        {/* Include appropriate error message if login is incorrect*/}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
